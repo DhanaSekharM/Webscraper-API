45c15200f428700c1e1bcfb895e9ffbc5d2d7be2
Dhanasekhar <sekhardhana529@gmail.com>
2019-06-08 16:36:49 +0530

    create table

---
+++ webscrape_the_one/the_one/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import ProductDetails
+
+
+class ProductDetailsSerializer(serializers.Serializer):
+    product_name = serializers.CharField(max_length=255)
+    product_url = serializers.CharField()
+    product_price = serializers.CharField(max_length=255)
+    all_time_low = serializers.CharField(max_length=255)
+
+    def create(self, validated_data):
+        return ProductDetails.objects.create(**validated_data)
