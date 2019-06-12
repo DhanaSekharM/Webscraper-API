3f78ea41f4ba610ca3f121e3bb81bda6b3b1c428
Dhanasekhar <sekhardhana529@gmail.com>
2019-06-12 16:43:26 +0530

    fixed product url

--- webscrape_the_one/the_one/views.py
+++ webscrape_the_one/the_one/views.py
@@ -11,38 +11,38 @@ from .models import ProductDetails
 from .main import main
 from .serializers import ProductDetailsSerializer
 from .Notifications import send_notifications
+from re import findall
 
 
 class Requests(APIView):
 
     def get(self, request):
-        # products = ProductDetails.objects.all()
-        # updated_products = []
-        # for product in products:
-        #     updated_product = ProductDetails.objects.get(id=product.id)
-        #     names, prices, rating = main(product=updated_product.product_url)
-        #     if prices[0] != updated_product.product_price:
-        #         updated_product.product_price = prices[0]
-        #     if float(prices[0]) <= float(updated_product.all_time_low):
-        #         updated_product.all_time_low = prices[0]
-        #     updated_product.save()
-        #     updated_products.append(updated_product)
+        products1 = ProductDetails.objects.get(id=11)
+        products = []
+        products.append(products1)
+        updated_products = []
+        for product in list(products):
+            updated_product = ProductDetails.objects.get(id=product.id)
+            names, prices, rating = main(product=updated_product.product_url)
+            if prices[0] != updated_product.product_price:
+                updated_product.product_price = prices[0]
+            if float(prices[0]) <= float(updated_product.all_time_low):
+                updated_product.all_time_low = prices[0]
+            updated_product.save()
+            updated_products.append(updated_product)
         # print(updated_products[5].product_name)
-        # data = ProductDetailsSerializer(updated_products, many=True)
-        #
-        # fcm_device = GCMDevice.objects.create(registration_id="f2Uu0kw0YQI:APA91bGCb5DZeMyoVBegEd75CVO9D4ovYedKqG4XmPSQjHzKIqdgSXNZv2TIPkKDZWsvW1mQp9POtpeRv78XI8XkFAQLxqMnXde9aVEokcbhEXxfiYZWXbapg882l5XKm3fQ9K76wVrp", cloud_message_type="FCM")
-        # fcm_device.send_message("This is a message")
-        #
-        # return JsonResponse(data.data, safe=False)
+        data = ProductDetailsSerializer(updated_products, many=True)
+
         send_notifications()
-        return JsonResponse({
-            'hello': 'worlsd',
-        })
+
+        return JsonResponse(data.data, safe=False)
 
     def post(self, request):
         jsonBody = json.loads(request.body)
         product = jsonBody['product']
-        names, price, rating = main(product=product)
+        url = findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]| [! * \(\),] | (?: %[0-9a-fA-F][0-9a-fA-F]))+', product)
+        product_url = url[0]
+        names, price, rating = main(product=product_url)
 
         # names = product_details[0]
         # price = product_details[1]
@@ -50,10 +50,12 @@ class Requests(APIView):
 
         data = {'product_name': names[0], 'price': price[0], 'rating': rating[0]}
 
-        new_product = ProductDetails(product_name=data['product_name'], product_url=product, product_price=data['price']
+        new_product = ProductDetails(product_name=data['product_name'], product_url=product_url,
+                                     product_price=data['price']
                                      , all_time_low=data['price'])
 
-        new_product.save()
+        # new_product.save()
+        print(product_url)
         data = ProductDetailsSerializer(new_product, many=False)
 
         return JsonResponse(data.data, safe=False)
